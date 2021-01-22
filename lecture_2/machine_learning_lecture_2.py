91edb576a117c68b707e19639838b56fdf4816cf
Marie-Isabelle Schonborn <marieisabelleschoenborn@gmail.com>
2021-01-22 12:25:02 +0100

    small correction

--- lecture_2/machine_learning_lecture_2.py
+++ lecture_2/machine_learning_lecture_2.py
@@ -18,29 +18,20 @@ with open(errors_file, 'r') as f:
 
 print(message)
 
-#titanic dataset
-
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv('titanic_train.csv')
-print(df.head())
-print(df.info())
-
 # Question 1 : How many people are in titanic and how many survivors?
 print(df['PassengerId'].count())
 print(df[df['Survived'] > 0]['Survived'].count())
 
 
 # Question 2 : How many that survived were female and how many that died were female?
-print(df[(df['Survived'] > 0) & (df['Sex'] == 'female')]['Survived'].count())
+print(df[(df['Survived'] == 1) & (df['Sex'] == 'female')]['Survived'].count())
 
 # Question 3 : How many children were on the titanic?
 print(df[df['Age'] < 19]['Age'].count())
 
 
 # Question 4 : How many children died that were on the ship?
-print(df[(df['Survived'] < 1) & (df['Age'] < 19)]['Survived'].count())
+print(df[(df['Survived'] == 0) & (df['Age'] < 19)]['Survived'].count())
 
 
 # Question 5 : How many people had families with them?
@@ -55,8 +46,3 @@ print(male / (female + male)) # percentage of males
 
 
 # Question 7 : What contributed to the survival of those who survived?
-
-
-
-
-
